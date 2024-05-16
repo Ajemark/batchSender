ee95dbf26a437919c34b0cd3d6f088279bb35bba
Ajemark <ajemark0110@gmail.com>
2024-05-16 13:50:44 +0100

    Batch ton sender

--- src/components/TransferTon.tsx
+++ src/components/TransferTon.tsx
@@ -3,14 +3,37 @@ import styled from "styled-components";
 import { Address, toNano } from "ton";
 import { useTonConnect } from "../hooks/useTonConnect";
 import { Card, FlexBoxCol, FlexBoxRow, Button, Input } from "./styled/styled";
+import { useTonConnectUI } from "@tonconnect/ui-react";
 
 export function TransferTon() {
   const { sender, connected } = useTonConnect();
 
-  const [tonAmount, setTonAmount] = useState("0.01");
-  const [tonRecipient, setTonRecipient] = useState(
-    "EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c"
-  );
+  const [tonConnectUI, setOptions] = useTonConnectUI();
+
+  const [tonAmount, setTonAmount] = useState("");
+  const [tonRecipient, setTonRecipient] = useState("");
+  const [formError, setformError] = useState(false);
+
+  const processTx = async function name() {
+    setformError(false);
+    if (tonAmount == "" || tonRecipient == "") {
+      setformError(true);
+      return;
+    }
+    let data: any = [];
+    tonRecipient.split("\n").map((d) => {
+      data.push({
+        address: d,
+        amount: toNano(tonAmount).toString(),
+      });
+    });
+
+    const transaction = {
+      validUntil: Date.now() + 5 * 60 * 1000,
+      messages: data,
+    };
+    const res = await tonConnectUI.sendTransaction(transaction);
+  };
 
   return (
     <Card>
@@ -21,31 +44,34 @@ export function TransferTon() {
           <Input
             style={{ marginRight: 8 }}
             type="number"
+            placeholder="Enter ton amount"
             value={tonAmount}
             onChange={(e) => setTonAmount(e.target.value)}
           ></Input>
         </FlexBoxRow>
         <FlexBoxRow>
           <label>To </label>
-          <Input
-            style={{ marginRight: 8 }}
+          <textarea
+            rows={10}
+            placeholder="Paste ddresses here"
+            style={{ marginRight: 8, width: "100%" }}
             value={tonRecipient}
             onChange={(e) => setTonRecipient(e.target.value)}
-          ></Input>
+          />
         </FlexBoxRow>
         <Button
           disabled={!connected}
           style={{ marginTop: 18 }}
-          onClick={async () => {
-            sender.send({
-              to: Address.parse(tonRecipient),
-              value: toNano(tonAmount),
-            });
-          }}
+          onClick={processTx}
         >
           Transfer
         </Button>
       </FlexBoxCol>
+      {formError && (
+        <p style={{ color: "red", textAlign: "center" }}>
+          Please Enter all fields
+        </p>
+      )}
     </Card>
   );
 }
